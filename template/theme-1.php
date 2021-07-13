21c8d35b988dd3987a25878c20a2a4ccc8e301b5
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-07-13 15:46:33 +0600

    markup update

---
+++ template/theme-1.php
@@ -0,0 +1,33 @@
+<!-- wpwax logo Grid Theme-1  -->
+
+<div class="wpwax-logo-showcase-wrap wpwax-logo-showcase-grid-1">
+
+    <div class="wpwax-logo-showcase-content">
+
+        <div class="wpwax-logo-showcase-item">
+            <img src="" alt="">
+        </div>
+
+        <div class="wpwax-logo-showcase-item">
+            <img src="" alt="">
+        </div>
+
+        <div class="wpwax-logo-showcase-item">
+            <img src="" alt="">
+        </div>
+
+        <div class="wpwax-logo-showcase-item">
+            <img src="" alt="">
+        </div>
+
+        <div class="wpwax-logo-showcase-item">
+            <img src="" alt="">
+        </div>
+
+        <div class="wpwax-logo-showcase-item">
+            <img src="" alt="">
+        </div>
+
+    </div>
+    
+</div>

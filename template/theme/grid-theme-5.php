83be4c8206bf7f7202d1913ac22bf1ae3cfde5f1
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-08-30 19:12:28 +0600

    grid 4 , grid 5 update

---
+++ template/theme/grid-theme-5.php
@@ -0,0 +1,15 @@
+
+        <div class="wpwax-lsu-item wpwax-lsu-item__borderless">
+
+            <div class="wpwax-lsu-item-inner">
+
+                <a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Conpany Name Sovware">
+
+                    <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
+                    
+                </a>
+
+            </div>
+
+        </div>
+

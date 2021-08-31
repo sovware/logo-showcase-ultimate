82901ccb4a8286db0d8f01226d06ba418816c608
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-08-31 17:31:19 +0600

    update

---
+++ template/theme/grid-theme-8.php
@@ -0,0 +1,15 @@
+
+<div class="wpwax-lsu-item wpwax-lsu-item__borderless">
+
+    <div class="wpwax-lsu-item-inner" data-bs-toggle="<?php echo ( 'yes' == $tooltip_show ) ? 'tooltip' : ''; ?>" data-bs-placement="top"
+        title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
+        
+        <a href="">
+
+            <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
+            
+        </a>
+
+    </div>
+
+</div>

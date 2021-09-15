9efcb2dd41dabc57aecf0012897ea20cfc673cb2
Tanjim Hasan <tanjim.hasan.pro@gmail.com>
2021-09-15 11:30:30 +0600

    update

--- classes/lcg-usages-support.php
+++ classes/lcg-usages-support.php
@@ -1,36 +1,38 @@
 <?php
 // usage and support view. It should be a nice page with tabbed content like the logo showcase plugin.
 ?>
-    <div class="lcs_support_wrapper">
+    <h1 class="wpcs-admin-header">Usage & Support</h1>
+    <div class="adl_support_wrapper">
+        <!--Support Block-->
         <div class="wrap wrap_left">
-            <div class="postbox lcs-upgrade-content-wrapper">
+            <div class="postbox wpcs-admin-extra">
+                <h3 class="wpcs-admin-extra-heading">Usage</h3>
+                <div class="postbox-inline-body">
+                    <p>1. Add as many logos as you want going Logo Showcase >> Add New Logo.</p>
+                    <p>2. Customize the settings for the logo going Logo Showcase >> Shortcode Generator.</p>
+                    <p>3. Use the shortcode of the Logo Showcase on any page or post to display the Logos.</p>
+                    <br>
 
-                <h3>Usage</h3>
-                <p>1. Add as many logos as you want going Logo Showcase >> Add New Logo.</p>
-                <p>2. Customize the settings for the logo going Logo Showcase >> Shortcode Generator.</p>
-                <p>3. Use the shortcode of the Logo Showcase on any page or post to display the Logos.</p>
-                <br>
+                    <h3>Support Forum</h3>
+                    <p> If you need any helps, please don't hesitate to post it on <a href="https://wordpress.org/support/plugin/logo-showcase-ultimate-carousel-slider-grid" target="_blank"> wordpress.org</a> or <a href="https://aazztech.com/support" target="_blank"> aazztech.com</a> support forum.</p>
+                    <br>
 
-                <h3>Support Forum</h3>
-                <p> If you need any helps, please don't hesitate to post it on <a href="https://wordpress.org/support/plugin/logo-showcase-ultimate-carousel-slider-grid" target="_blank"> wordpress.org</a> or <a href="https://aazztech.com/support" target="_blank"> aazztech.com</a> support forum.</p>
-                <br>
+                    <h3>More features</h3>
+                    <p>Upgrading to the <a href="https://aazztech.com/product/logo-showcase-ultimate-pro/" target="_blank">Premium Version</a> would unlock more amazing features of the plugin.</p>
+                    <p>
+                        <a class="button button-primary" href="https://aazztech.com/demos/plugins/logo-showcase-ultimate-pro-screenshots/" target="_blank">Backend Screenshots</a>
 
-                <h3>More features</h3>
-                <p>Upgrading to the <a href="https://aazztech.com/product/logo-showcase-ultimate-pro/" target="_blank">Premium Version</a> would unlock more amazing features of the plugin.</p>
-                <p>
-                    <a class="button button-primary" href="https://aazztech.com/demos/plugins/logo-showcase-ultimate-pro-screenshots/" target="_blank">Backend Screenshots</a>
+                        <a class="button button-primary" href="https://aazztech.com/demos/plugins/logo-showcase-ultimate-carousel-slider-grid/" target="_blank">Live Preview</a>
 
-                    <a class="button button-primary" href="https://aazztech.com/demos/plugins/logo-showcase-ultimate-carousel-slider-grid/" target="_blank">Live Preview</a>
-
-                    <a class="button button-primary" href="https://aazztech.com/product/logo-showcase-ultimate-pro/" target="_blank">Upgrade to Pro</a>
-                </p>
+                        <a class="button button-primary" href="https://aazztech.com/product/logo-showcase-ultimate-pro/" target="_blank">Upgrade to Pro</a>
+                    </p>
+                </div>
             </div>
-
         </div>
 
-
+        <!--Upgrade Block-->
         <div class="wrap wrap_right">
-            <div class="postbox lcs-upgrade-content-wrapper-right">
+            <div class="postbox wpcs-admin-extra adl-upgrade-content-wrapper-right">
                 <h3>Need More Features? </h3>
                 <ul class="lcs_pro_features">
                     <li>24 different themes.</li>
@@ -47,7 +49,7 @@
                     <li>Display logo in your desired order with easy Drag & Drop functionality <span class="lcg_new_feature">New</span></li>
                     <li>Short description text color change option</li>
                 </ul>
-                <p class="text-center"><a target="_blank" href="https://aazztech.com/product/logo-showcase-ultimate-pro/" class="button button-primary">Get the Pro Version Now!</a></p>
+                <p class="text-center"><a target="_blank" href="https://aazztech.com/product/logo-showcase-ultimate-pro/" class="button button-secondary">Get the Pro Version Now!</a></p>
             </div>
         </div>
     </div>

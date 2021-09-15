f1a78552dc5139de6ebcb4cd8a7ad1aae9c22f4d
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-09-15 16:33:34 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@
         let swiper = new Swiper(`.wpwax-lsu-carousel-${i}`, {
             slidesPerView: checkData(parseInt(el.dataset.lsuItems), 4),
             spaceBetween: checkData(parseInt(el.dataset.lsuMargin), 30),
-            loop: checkData(el.dataset.lsuLoop, true),
+            loop: checkData(JSON.parse(el.dataset.lsuLoop), true),
             slidesPerGroup: checkData(parseInt(el.dataset.lsuPerslide), 4),
             speed: checkData(parseInt(el.dataset.lsuSpeed), 3000),
             autoplay: checkData(JSON.parse(el.dataset.lsuAutoplay), {}),

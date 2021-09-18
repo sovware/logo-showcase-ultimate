60d85aa2dde1dc01b95982bc706f8f31656ad8e6
Tanjim Hasan <tanjim.hasan.pro@gmail.com>
2021-09-18 10:10:30 +0600

    update

--- template/theme/grid-theme-1.php
+++ template/theme/grid-theme-1.php
@@ -1,8 +1,8 @@
 <div class="wpwax-lsu-item wpwax-lsu-item-bordered">
 
-    <div class="wpwax-lsu-item-inner">
+    <div class="wpwax-lsu-item-inner"  data-bs-toggle="<?php echo ( 'yes' == $tooltip_show ) ? 'tooltip' : ''; ?>" data-bs-placement="<?php echo $tooltip_posi; ?>" title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
 
-        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>" data-bs-toggle="<?php echo ( 'yes' == $tooltip_show ) ? 'tooltip' : ''; ?>" data-bs-placement="<?php echo $tooltip_posi; ?>" title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
+        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
 
             <img src="<?php echo ! empty( $lcg_img ) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
 
--- template/theme/grid-theme-5.php
+++ template/theme/grid-theme-5.php
@@ -1,9 +1,9 @@
 
         <div class="wpwax-lsu-item wpwax-lsu-item-borderless">
 
-            <div class="wpwax-lsu-item-inner">
+            <div class="wpwax-lsu-item-inner" data-bs-toggle="tooltip" data-bs-placement="<?php echo $tooltip_posi; ?>" title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
 
-                <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>" data-bs-toggle="tooltip" data-bs-placement="<?php echo $tooltip_posi; ?>" title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
+                <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
 
                     <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
                     

9c893ec0a0665c840503f5bfbf26daffe71313ea
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-09-19 08:33:43 +0600

    update

--- template/theme/grid-theme-3.php
+++ template/theme/grid-theme-3.php
@@ -1,9 +1,9 @@
 
 <div class="wpwax-lsu-item">
 
-    <div class="wpwax-lsu-item-inner">
+    <div class="wpwax-lsu-item-inner" data-bs-toggle="tooltip" data-bs-placement="top" title="Conpany Name Sovware"
 
-        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>" data-bs-toggle="tooltip" data-bs-placement="top" title="Conpany Name Sovware">
+        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
 
             <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
             

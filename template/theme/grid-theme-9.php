1957706aeb736d40aa9720e1771f936b1a9fece1
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-09-19 08:29:41 +0600

    update

--- template/theme/grid-theme-9.php
+++ template/theme/grid-theme-9.php
@@ -1,20 +1,20 @@
 
 <div class="wpwax-lsu-item wpwax-lsu-item-bordered wpwax-lsu-item-flip-card">
 
-<div class="wpwax-lsu-item-inner" data-bs-toggle="<?php echo ( 'yes' == $tooltip_show ) ? 'tooltip' : ''; ?>" data-bs-placement="<?php echo $tooltip_posi; ?>" title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
-    
-    <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>" class="wpwax-lsu-item-flip-card__front">
-
-        <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
+    <div class="wpwax-lsu-item-inner" data-bs-toggle="<?php echo ( 'yes' == $tooltip_show ) ? 'tooltip' : ''; ?>" data-bs-placement="<?php echo $tooltip_posi; ?>" title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
         
-    </a>
+        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>" class="wpwax-lsu-item-flip-card__front">
 
-    <div class="wpwax-lsu-item-flip-card__back">
+            <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
+            
+        </a>
 
-        <h4 class="wpwax-lsu-item-flip-card__back--content"><?php the_title(); ?></h4>
-        
+        <div class="wpwax-lsu-item-flip-card__back">
+
+            <h4 class="wpwax-lsu-item-flip-card__back--content"><?php the_title(); ?></h4>
+            
+        </div>
+
     </div>
-
-</div>
 
 </div>

1c8172a014e2df2220bfd60c7f3f82b4242782e4
Miroslav Matějů <melebius@gmail.com>
2021-10-27 09:45:45 +0200

    fix empty link
    An empty href attribute creates a link leading to the current page.
    Remove the <a> tag completely instead.
    
    Also removed useless whitespace at the end of files.

--- template/theme/carousel-theme-2.php
+++ template/theme/carousel-theme-2.php
@@ -4,14 +4,16 @@
     <div class="wpwax-lsu-item-inner" data-bs-toggle="<?php echo ( 'yes' == $tooltip_show ) ? 'tooltip' : ''; ?>" data-bs-placement="top"
         title="<?php echo ! empty( $tooltip ) ? $tooltip : ''; ?>">
         
-        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
+    <?php if ( ! empty( $img_link ) ) { ?>
+        <a href="<?php echo $img_link ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
+    <?php } ?>
 
             <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
             
+    <?php if ( ! empty( $img_link ) ) { ?>
         </a>
+    <?php } ?>
 
     </div>
 
 </div>
-
-      
--- template/theme/grid-theme-3.php
+++ template/theme/grid-theme-3.php
@@ -3,14 +3,16 @@
 
     <div class="wpwax-lsu-item-inner">
 
-        <a href="<?php echo ! empty( $img_link ) ? $img_link : ''; ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
+    <?php if ( ! empty( $img_link ) ) { ?>
+        <a href="<?php echo $img_link ?>" target="<?php echo ! empty( $target ) ? $target : ''; ?>">
+    <?php } ?>
 
             <img src="<?php echo !empty($lcg_img) ? $lcg_img : ''; ?>" alt="<?php the_title(); ?>">
             
+    <?php if ( ! empty( $img_link ) ) { ?>
         </a>
+    <?php } ?>
 
     </div>
 
 </div>
-
-       

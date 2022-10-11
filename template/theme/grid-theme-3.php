cb830c2d9d6aecb960b3620f3ee4b0e66413422f
Armanul46 <aazztech4@gmail.com>
2022-10-11 07:08:53 +0600

    fixed style issue

--- template/theme/grid-theme-3.php
+++ template/theme/grid-theme-3.php
@@ -3,7 +3,7 @@
 
     <div class="wpwax-lsu-item-inner">
 
-        <a href="<?php echo ! empty( $img_link ) ? esc_url( $img_link ) : ''; ?>" target="<?php echo ! empty( $target ) ? esc_url( $target ) : ''; ?>">
+        <a href="<?php echo ! empty( $img_link ) ? esc_url( $img_link ) : ''; ?>" target="<?php echo ! empty( $target ) ? esc_attr( $target ) : ''; ?>">
 
             <img src="<?php echo ! empty( $lcg_img ) ? esc_url( $lcg_img ) : ''; ?>" alt="<?php echo esc_url( get_the_title() ); ?>">
             

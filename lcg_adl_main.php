4ccc60b2d3fedcc4037e18f2c6d5093a152f2adc
Armanul46 <aazztech4@gmail.com>
2022-11-28 10:07:20 +0600

    fixed gutenberg file enqueue issue

--- lcg_adl_main.php
+++ lcg_adl_main.php
@@ -61,6 +61,8 @@ if ( ! class_exists( 'Lcg_Main_Class' ) ) {
                 add_action( 'elementor/preview/enqueue_styles', [ self::$instance, 'elementor_enqueue_preview_style' ] );
                 add_action( 'elementor/preview/enqueue_scripts', [ self::$instance, 'elementor_preview_enqueue_script' ] );
 
+                add_action( 'enqueue_block_editor_assets', [ self::$instance, 'enqueue_block_editor_assets' ] );
+
                 // support svg format
                 add_filter( 'upload_mimes', array( self::$instance, 'lcg_support_svg' ) );
                 if( empty( get_option('lcg_dismiss_notice') ) ) {
@@ -133,7 +135,17 @@ if ( ! class_exists( 'Lcg_Main_Class' ) ) {
             wp_enqueue_script('lcg-tooltip-js', LCG_PLUGIN_URI . '/assets/js/vendor/tooltip.js', array('jquery', 'lcg-popper-js'));
             wp_enqueue_script('lcg-swiper-min-js', LCG_PLUGIN_URI . '/assets/js/vendor/swiper-bundle.min.js', array('jquery', 'lcg-tooltip-js'));
             wp_enqueue_script('main-js', LCG_PLUGIN_URI . '/assets/js/main.js', array('jquery', 'lcg-swiper-min-js'));
-            wp_enqueue_script('ajax-js', LCG_PLUGIN_URI . '/assets/js/ajax.js', array('jquery'));
+        }
+
+        public function enqueue_block_editor_assets() {
+            wp_enqueue_style('lcg-style', LCG_PLUGIN_URI . '/assets/css/style.css');
+            wp_enqueue_style('lcg-swiper-min-css', LCG_PLUGIN_URI . '/assets/css/vendor/swiper-bundle.min.css');
+            wp_enqueue_style('lcg-tooltip', LCG_PLUGIN_URI . '/assets/css/vendor/tooltip.css');
+            
+            wp_enqueue_script('lcg-popper-js', LCG_PLUGIN_URI . '/assets/js/vendor/popper.min.js', array('jquery'));
+            wp_enqueue_script('lcg-tooltip-js', LCG_PLUGIN_URI . '/assets/js/vendor/tooltip.js', array('jquery', 'lcg-popper-js'));
+            wp_enqueue_script('lcg-swiper-min-js', LCG_PLUGIN_URI . '/assets/js/vendor/swiper-bundle.min.js', array('jquery', 'lcg-tooltip-js'));
+            wp_enqueue_script('main-js', LCG_PLUGIN_URI . '/assets/js/main.js', array('jquery', 'lcg-swiper-min-js'));
         }
 
         public function lcg_load_textdomain() {
